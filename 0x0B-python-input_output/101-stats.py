f5867a5739360f22f07c8962103ab075dc7049ee
khunoane <ntsohak@gmail.com>
2023-01-10 04:26:20 -0800

    101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -44,7 +44,7 @@ if __name__ == "__main__":
             try:
                 if line[-2] in valid_codes:
                     if status_codes.get(line[-2], -1) == -1:
-status_codes[line[-2]] = 1
+                        status_codes[line[-2]] = 1
                     else:
                         status_codes[line[-2]] += 1
             except IndexError:
